e27726ec31b736eef05dc29c46028335175e5753
Dezolder <evgeniy.zhukov.2012@gmail.com>
2024-05-25 21:16:48 +0300

    Ready: Volumes is ready

--- kubernetes-volumes/bash.sh
+++ kubernetes-volumes/bash.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-# Precondition for this script - Docker with clean minikube is installed
+# Precondition for this script 
+# - Docker with clean minikube is installed
+# - host resolution: Use PowerShell as Administrator:
+#   notepad C:\Windows\System32\drivers\etc\hosts
+#   127.0.0.1 homework.otus
+# - Start minikube
+#   minikube start
 
-# Start minikube
-# minikube start
+# Set context
 kubectl config set-context --current --namespace=homework
+
 # Set Node label
 kubectl label node minikube homework=true
 
 # Apply homework manifests
 kubectl apply -f namespace.yaml &&
 kubectl apply -f cm.yaml &&
+kubectl apply -f storageClass.yaml &&
 kubectl apply -f pvc.yaml 
 kubectl apply -f deployment.yaml &&
 kubectl apply -f service.yaml &&
 kubectl apply -f ingress.yaml &&
 
 # Enable minikube ingress
-# minikube addons enable ingress &&
-
-# sleep 30
+minikube addons enable ingress &&
 
 # Make tunnel to minikube in detached mode
 # Please note, that minikube service nginx-service - doesn't work!
@@ -27,10 +32,7 @@ minikube tunnel --cleanup > minikube-tunnel.log 2>&1 &
 
 sleep 20
 
-# host resolution
-# Use PowerShell as Administrator
-# notepad C:\Windows\System32\drivers\etc\hosts
-# 127.0.0.1 homework.otus
+
 
 # Test the service in a second terminal:
 curl http://homework.otus/homepage | grep "Welcome to our website!" --color
@@ -42,9 +44,16 @@ curl http://homework.otus/conf/key | grep "value" --color
 # value
 
 # Cleanup
-# minikube addons disable ingress &&
-# kubectl delete -f ingress.yaml &&
-# kubectl delete -f service.yaml &&
-# kubectl delete -f deployment.yaml &&
-# kubectl delete -f namespace.yaml &&
-# kubectl label node minikube homework-
+minikube addons disable ingress &&
+kubectl delete -f ingress.yaml &&
+kubectl delete -f service.yaml &&
+kubectl delete -f deployment.yaml &&
+kubectl delete -f cm.yaml &&
+kubectl delete -f storageClass.yaml &&
+kubectl delete -f namespace.yaml &&
+kubectl label node minikube homework-
+
+
+# kubectl delete -f pvc.yaml - PV удаляется вместе с Deployment
+
+kubectl get pv | grep "Released" --color
